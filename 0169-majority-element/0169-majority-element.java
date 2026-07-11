6adab454fdef0c8a82a11d20aa3974dc53f9d355
sagargherade55 <sagargherade55@gmail.com>
2026-07-11 23:09:29 +0530

    Time: 2 ms (70.26%), Space: 55.6 MB (59.69%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        
+        int candidate=0, cnt=0;
+
+        for(int num : nums){
+            if(cnt==0){
+                candidate=num;
+            }
+            if(candidate==num){
+                cnt++;
+            }else{
+                cnt--;
+            }
+        }
+        return candidate;
+    }
+}
