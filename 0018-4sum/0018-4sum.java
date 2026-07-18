36fcc60713524a70036e513358c60103d3a50114
sagargherade55 <sagargherade55@gmail.com>
2026-07-18 22:38:26 +0530

    Time: 19 ms (83.69%), Space: 46.1 MB (22.1%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -20,10 +20,11 @@ class Solution {
                     if(sum == target){
                         ans.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
 
-                        while(left< right && nums[left]== nums[left+1]) left++;
-                        while(left< right && nums[right]== nums[right-1]) right--;
                         left++;
                         right--;
+                        while(left< right && nums[left]== nums[left-1]) left++;
+                        while(left< right && nums[right]== nums[right+1]) right--;
+                    
                     }else if(sum< target) left++;
                     else right--;
                 }
